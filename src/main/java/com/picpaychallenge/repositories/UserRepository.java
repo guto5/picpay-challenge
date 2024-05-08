7d1f17f7aacabf84c47b29fd23ca50320733bd9f
guto5 <guto.cezar2005@gmail.com>
2024-05-08 17:35:22 -0300

    feat: create User Repository

---
+++ src/main/java/com/picpaychallenge/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.picpaychallenge.repositories;
+
+import com.picpaychallenge.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+
+}
