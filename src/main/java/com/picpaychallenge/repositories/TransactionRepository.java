7c468ecec89d51221b384c266cfa1ea2a7c4c48f
guto5 <guto.cezar2005@gmail.com>
2024-05-08 17:37:30 -0300

    feat: create Transaction Repository

---
+++ src/main/java/com/picpaychallenge/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaychallenge.repositories;
+
+import com.picpaychallenge.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
