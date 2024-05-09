47ef9700a113d6a1dcebc72a93f25a268a391948
guto5 <guto.cezar2005@gmail.com>
2024-05-09 13:29:28 -0300

    feat: create Full transaction structure

---
+++ src/main/java/com/picpaychallenge/controllers/TransactionController.java
@@ -0,0 +1,27 @@
+package com.picpaychallenge.controllers;
+
+import com.picpaychallenge.domain.transaction.Transaction;
+import com.picpaychallenge.dtos.TransactionDTO;
+import com.picpaychallenge.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transactionDTO) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransaction(transactionDTO);
+        return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
+
+    }
+}
